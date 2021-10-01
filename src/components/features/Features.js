7ff2f3c9d6cd927cb974a26faebf737a57f71e89
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2021-10-02 03:09:05 +0530

    completed the page

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -23,6 +23,50 @@ const Features = () => {
           </div>
           <div className="right_inner_content">
             <h1 className="section_heading features_heading">Features</h1>
+            <div className="features_content">
+              <ol className="ordered_list">
+                <li className="list">
+                  Product Range
+                  <ul className="inner_unordered_list">
+                    We have a 20 Categories with more than 360 products
+                  </ul>
+                </li>
+                <li className="list">
+                  Quality
+                  <ul className="inner_unordered_list">
+                    Fresh, Hygienic And Sanitized Products
+                  </ul>
+                </li>
+                <li className="list">
+                  Great Deals
+                  <ul className="inner_unordered_list">
+                    Stunning And Exciting Offers on all products throughout the
+                    year.
+                  </ul>
+                </li>
+                <li className="list">
+                  Easy to order
+                  <ul className="inner_unordered_list">
+                    Simply Signup, add the products you need in the cart, and
+                    checkout. We'll deliver to your doorstep.
+                  </ul>
+                </li>
+                <li className="list">
+                  Fastest Delivery
+                  <ul className="inner_unordered_list">
+                    We deliver the orders within a day (24hrs from the time of
+                    purchase)
+                  </ul>
+                </li>
+                <li className="list">
+                  Support And Easy Refund
+                  <ul className="inner_unordered_list">
+                    We are always ready to solve your concerns about the refund
+                    And product issues easily.
+                  </ul>
+                </li>
+              </ol>
+            </div>
           </div>
           <div className="features_bottom_image">
             <img src={bgCityImage} alt="" className="bg_city_image" />
--- src/components/whysoulbite/WhySoulbite.js
+++ src/components/whysoulbite/WhySoulbite.js
@@ -15,57 +15,24 @@ const WhySoulbite = () => {
             <h1 className="section_heading why_soulbite_section_heading">
               WHY SOULBITE ?
             </h1>
-          </div>
-          <div>
-            <ol className="ordered_list">
-              <li className="list">
-                Product Range
-                <ul className="inner_unordered_list">
-                  We have a 20 Categories with more than 360 products
-                </ul>
-              </li>
-              <li className="list">
-                Quality
-                <ul className="inner_unordered_list">
-                  Fresh, Hygienic And Sanitized Products
-                </ul>
-              </li>
-              <li className="list">
-                Great Deals
-                <ul className="inner_unordered_list">
-                  Stunning And Exciting Offers on all products throughout the
-                  year.
-                </ul>
-              </li>
-              <li className="list">
-                Easy to order
-                <ul className="inner_unordered_list">
-                  Simply Signup, add the products you need in the cart, and
-                  checkout. We'll deliver to your doorstep.
-                </ul>
-              </li>
-              <li className="list">
-                Fastest Delivery
-                <ul className="inner_unordered_list">
-                  We deliver the orders within a day (24hrs from the time of
-                  purchase)
-                </ul>
-              </li>
-              <li className="list">
-                Support And Easy Refund
-                <ul className="inner_unordered_list">
-                  We are always ready to solve your concerns about the refund
-                  And product issues easily.
-                </ul>
-              </li>
-            </ol>
-          </div>
-          <div className="why_soulbite_left_bottom_image">
-            <img src={biscuitsImage} alt="" className="biscuits_image" />
+            <div className="why_soulbite_content">
+              Discover the new products of your daily needs from your
+              comfortable place and We are so passionate to provide the best
+              services to our customers.
+            </div>
+            <div className="why_soulbite_content why_soulbite_content_section_two">
+              Our prime focus area is Freshness And Hygiene to maintain customer
+              satisfaction and we constantly monitor shopping trends to react
+              quickly And effectively based on customer suggestions and
+              feedback.
+            </div>
+            <div className="why_soulbite_left_bottom_image">
+              <img src={biscuitsImage} alt="" className="biscuits_image" />
+            </div>
           </div>
         </div>
         <div className="why_soulbite_right_inner">
-          <div className="why_soulbite_right_inner_rounded">
+          <div className="rounded_inner">
             <img src={milkImage} alt="" className="milk_image" />
           </div>
         </div>
